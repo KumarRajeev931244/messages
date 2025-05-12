eba92c90cdfd1e411bec17b60ac91b390aea1e71
rajeev <rajeevkumar25112002@gmail.com>
2025-05-12 11:34:43 +0530

    refactor: clean up imports and improve code structure in verification page

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,84 +1,79 @@
 'use client'
-import { verifySchema } from "@/schemas/verifySchema"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useParams, useRouter } from "next/navigation"
-import { useForm} from "react-hook-form"
-import { toast } from "sonner"
-import { z } from "zod"
-import axios, { AxiosError } from "axios"
-import { ApiResponse } from "@/types/ApiResponse"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { useParams, useRouter } from 'next/navigation'
+import React from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosError } from 'axios'
+import { toast } from 'sonner'
+import { ApiResponse } from '@/types/ApiResponse'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
 import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
+import { verifySchema } from '@/schemas/verifySchema'
 
-const page = () => {
-    const router = useRouter()
-    const param = useParams<{username: string}>()
+
+const Page = () => {
+    const router =  useRouter()
+    // getting data from url
+    const params = useParams<{username: string}>()
     const form = useForm<z.infer<typeof verifySchema>>({
-      resolver: zodResolver(verifySchema),
-    });
-    const onSubmit = async(data: z.infer<typeof verifySchema>) =>{
+        resolver: zodResolver(verifySchema)
+    })
+    const onSubmit = async(data: z.infer<typeof verifySchema>) => {
+       
         try {
-            console.log('verify code data:',data);
-            
-            const response = await axios.post('/api/verify-code',{
-                username: param.username,
+            const response = await axios.post<ApiResponse>(`/api/verify-code`, {
+                username: params.username,
                 code: data.code
             })
-            console.log("verify response:", response);
-            
-            toast.success(response.data.message)
-            router.replace(`/signIn`)
+            toast("success",{
+                description: response.data.message
+            })
+            router.replace('/sign-in')
         } catch (error) {
-            console.error("Error in verify code", error);
-            const axiosError = error as AxiosError<ApiResponse>;
-            console.log("axiosError:", axiosError);
-            let errorMessage = axiosError.response?.data.message;
-            toast.error(errorMessage);
+            console.error("error in signup of user", error)
+            const axiosError = error as AxiosError<ApiResponse>
+            let errorMessage = axiosError.response?.data.message
+           toast.error(errorMessage)
         }
-        return (
-          <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-              <div className="text-center">
-                <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                  Verify your account
-                </h1>
-                <p className="mb-4">
-                  Enter the verification code sent to your email
-                </p>
-              </div>
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-4"
-                >
-                  <FormField
-                    name="code"
-                    control={form.control}
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>verification code</FormLabel>
-                        <FormControl>
-                          <Input placeholder="code" {...field} />
-                        </FormControl>
-
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button
-                    type="submit"
-                    className="cursor-pointer"
-                    onClick={() => {}}
-                  >
-                    submit
-                  </Button>
-                </form>
-              </Form>
-            </div>
-          </div>
-        );
-        
     }
+    return (
+       <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+            <div className="text-center">
+                <h1 className="text-4xl font-extrabold tacking-light lg:text-5xl mb-6">verify your account</h1>
+                <p className="mb-4"> Enter the verification code sent to your email</p>                 
+            </div>
+            <div className="text-center">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+                        <FormField
+                            name='code'
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>verification code</FormLabel>
+                                    <FormControl>
+                                        
+                                        <Input placeholder='code' {...field}/>
+ 
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        >
+                        </FormField>
+                        <Button className='cursor-pointer' type='submit'>Submit</Button>
+                    </form>
+                </Form>
+            
+            </div>
+        </div>
+
+       </div>
+    )
 }
-export default page
+
+
+export default Page
